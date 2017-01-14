c248814d7ab25966fee986dee082d853cae37b40
I K <ik@Is-iMac.local>
2017-01-14 15:19:58 -0500

    Added update

---
+++ playground/mongodb-update.js
@@ -0,0 +1,18 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server.');
+  }
+  console.log('Connected to MongoDB server');
+
+
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('587a7976ca345c1b47519469')
+  }, {$set: {name:'Jennifer'},$inc: {age:1}}, {returnOriginal:false}).then((result) => {
+    console.log(result);
+  })
+  //db.close();
+});
